72ce674529255e488978163dd6b759f65b14cee0
Tim Paine <t.paine154@gmail.com>
2019-09-05 17:09:30 -0400

    updates

--- nbcx_templates/tags.py
+++ nbcx_templates/tags.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 
 class Tags(Enum):
+    DISCLOSURES = 'nbcx_disclosures'
     NBCONVERT_IGNORE = 'nbcx_ignore'
     PARAMETERS = 'parameters'  # for Papermill
     TITLE = 'nbcx_title'
