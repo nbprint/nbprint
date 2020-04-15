3fab7c886e665af7dfcce7ce859418e4955fd7b3
Tim Paine <t.paine154@gmail.com>
2020-04-15 17:14:21 -0400

    remove document

--- nbcx_templates/document/__init__.py
+++
@@ -1,1 +0,0 @@
-from .document import *  # noqa: F401, F403
--- nbcx_templates/document/parts/__init__.py
+++
@@ -1,4 +0,0 @@
-from .appendix import *  # noqa: F401, F403
-from .diagram import *  # noqa: F401, F403
-from .table import *  # noqa: F401, F403
-from .title import *  # noqa: F401, F403
