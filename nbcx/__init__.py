4c82a75fac7ef563858521b739a9e350bc35290e
Tim Paine <t.paine154@gmail.com>
2020-04-16 10:57:44 -0400

    remove config, add new api docs

--- nbcx/__init__.py
+++ nbcx/__init__.py
@@ -1,5 +1,5 @@
 from ._version import __version__  # noqa: F401
 
-
-from .tags import *  # noqa: F403, F401
+from .exporters import *  # noqa: F403, F401
 from .utils import *  # noqa: F403, F401
+from .tags import *  # noqa: F403, F401
