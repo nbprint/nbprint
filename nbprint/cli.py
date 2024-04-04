afa7cf2e259ea5890294cfe2c05285f0e55f2a1c
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2024-04-04 17:31:53 -0400

    switch to hydra as default, queue up more interesting inline example

--- nbprint/cli.py
+++ nbprint/cli.py
@@ -9,21 +9,8 @@ def cli(path: Path, name: str):
     config.run()
 
 
-def cli_hydra(path: Path, name: str):
-    path = path.resolve()
-    config = Configuration.load_hydra(str(path.parent), str(path.name), name)
-    config.run()
-
-
 def main():
     app = Typer()
     app.command("run")(cli)
-    # TODO more
-    app()
-
-
-def main_hydra():
-    app = Typer()
-    app.command("run")(cli_hydra)
     # TODO more
     app()
--- nbprint/example/basic/page.py
+++ nbprint/example/basic/page.py
@@ -1,9 +1,16 @@
+import os.path
 from IPython.display import HTML, Image
-from pydantic import Field
+from pydantic import Field, FilePath
 
-from nbprint import ContentImage, PageRegion, PageRegionContent
+from nbprint import ContentImage, Page, PageRegion, PageRegionContent
 
 from .cover import ExampleNBPrintLogo
+
+__all__ = (
+    "ExampleLogoInFooter",
+    "ExampleBookIcon",
+    "ExampleReportPage",
+)
 
 
 class ExampleLogoInFooter(PageRegion):
@@ -25,3 +32,36 @@ class ExampleLogoInFooter(PageRegion):
         else:
             img = Image(data=self.logo.content)
         return HTML(f"""<img class="footer-logo" src="data:image/png;base64,{img._repr_png_()}">""")
+
+
+class ExampleBookIcon(ContentImage):
+    path: FilePath = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "..", "..", "..", "examples", "assets", "book.png")
+    )
+
+
+class ExampleReportPage(Page):
+    bottom_left: PageRegion = Field(default=ExampleLogoInFooter())
+    bottom: PageRegion = Field(default=PageRegion(content=PageRegionContent(content="Example Report")))
+    bottom_right: PageRegion = Field(default=PageRegion())
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.css += f"""
+.pagedjs_first_page {{
+    background-image: url("data:image/png;base64,{ExampleBookIcon().as_base64()}");
+    background-size: 50%;
+    background-repeat: no-repeat;
+    background-position-y: 75%;
+    background-position-x: 50%;
+}}
+
+.pagedjs_first_page div.pagedjs_margin-bottom {{
+    display: none;
+}}
+
+.pagedjs_margin-bottom-center > pagedjs_margin-content::after {{
+    font-weight: bold;
+}}
+"""
