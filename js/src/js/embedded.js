06139b0659e5e9b0c8620e6ccd178cac17b61944
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2024-05-27 18:31:33 -0400

    Some formatting and layout changes, tweaks to nbprint build system in js

--- js/src/js/embedded.js
+++ js/src/js/embedded.js
@@ -1,22 +1,16 @@
 import { initializeNBPrint } from "./nbprint";
-import { build } from "./nbconvert";
 import "@fortawesome/fontawesome-free/js/all";
+
+let nbprint = initializeNBPrint();
 
 document.addEventListener("DOMContentLoaded", async () => {
   // Process with NBPrint
-  let nbprint = initializeNBPrint();
 
   await nbprint.process();
 
   if (nbprint.buildPagedJS()) {
     // Build pagedjs
-    if (window.voila_process !== undefined) {
-      setTimeout(async () => {
-        await build();
-      }, 3000);
-    } else {
-      await build();
-    }
+    await nbprint.build();
   }
 
   await nbprint.postprocess();
--- nbprint/config/content/__init__.py
+++ nbprint/config/content/__init__.py
@@ -2,6 +2,6 @@ from .base import Content, ContentCode, ContentMarkdown
 from .common import TextComponent
 from .cover import ContentCover
 from .image import ContentImage
-from .page import ContentFlexColumnLayout, ContentFlexRowLayout
+from .page import *
 from .pagebreak import ContentPageBreak
 from .table_of_contents import ContentTableOfContents
