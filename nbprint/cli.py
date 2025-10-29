1088feebbb4465d41a37c366d4c3cbc312f2c14b
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-10-29 16:12:54 -0400

    Make top level config a callable model, allow direct notebook running, don't create output directory when running in notebook

--- nbprint/cli.py
+++ nbprint/cli.py
@@ -1,8 +1,10 @@
 from pathlib import Path
+from pprint import pprint
 from typing import Optional
 
 from hydra import compose, initialize_config_dir
 from hydra.utils import instantiate
+from nbformat import read as nb_read
 from omegaconf import OmegaConf
 from typer import Argument, Typer
 
@@ -35,21 +37,30 @@ def run_hydra(
     # prune any empty strings
     overrides = [o for o in overrides if o]
 
+    # If its a notebook, parse it out and run directly
+    # Read notebook contents and shove into config
+    config_name = "nbprint/config/default.yaml" if path.suffix in (".ipynb",) else str(path.name)
+
     with initialize_config_dir(config_dir=str(path.parent.absolute()), version_base=None):
-        hydra_config = compose(config_name=str(path.name), overrides=overrides)
+        hydra_config = compose(config_name=config_name, overrides=overrides)
 
         # bridge hydra and non-hydra
         extras = {"name": path.name.replace(".yaml", "")} if "name" not in hydra_config else {}
+
+        # swap in notebook content, if needed
+        if path.suffix in (".ipynb",):
+            with path.open("r", encoding="utf-8") as path_file:
+                nb_content = nb_read(path_file, as_version=4)
+            if "content" not in extras:
+                extras["content"] = []
+            for cell in nb_content.cells:
+                extras["content"].append({"_target_": "nbprint.ContentCode" if cell.cell_type == "code" else "nbprint.ContentMarkdown", "source": cell.source})
         config = instantiate(hydra_config, **extras)
         if not isinstance(config, Configuration):
             config = Configuration.model_validate(config)
 
     # mimic hydra cfg
-    if cfg:
-        print(OmegaConf.to_yaml(config.model_dump(mode="json")))  # noqa: T201
-    else:
-        # TODO: return file?
-        config.run(dry_run=dry_run)
+    pprint(OmegaConf.to_yaml(config.model_dump(mode="json"))) if cfg else config.run(dry_run=dry_run)
     return config
 
 
