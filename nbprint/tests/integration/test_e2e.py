87e5b765cee1a23f736e11ca26c962007960daa1
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-11-04 16:37:40 -0500

    Add path for notebook, add ccflow tests

--- nbprint/tests/integration/test_e2e.py
+++ nbprint/tests/integration/test_e2e.py
@@ -1,9 +1,11 @@
 import os.path
+import sys
+from unittest.mock import patch
 
 import pytest
 
 from nbprint import Configuration
-from nbprint.cli import run
+from nbprint.cli import hydra, run
 
 
 def _example_folder_does_not_exist():
@@ -36,3 +38,32 @@ def test_run_notebook_direct(template):
         run(f"examples/{template}.ipynb", ["+parameters.a=10", "+parameters.b='hello'", "+parameters.c=True"])
     else:
         run(f"examples/{template}.ipynb", [])
+
+
+@pytest.mark.skipif(_example_folder_does_not_exist(), reason="Examples - notebook direct")
+@pytest.mark.parametrize("parameters", ("string1", "string2"))
+def test_run_notebook_ccflow(parameters):
+    argv = [
+        "nbprint",
+        "--config-dir",
+        "examples/ccflow",
+        "nbprint=inline",
+        f"+context={parameters}",
+    ]
+    with patch.object(sys, "argv", argv):
+        hydra()
+
+
+@pytest.mark.skipif(_example_folder_does_not_exist(), reason="Examples - notebook direct")
+@pytest.mark.parametrize("parameters", ("string1", "string2"))
+def test_run_notebook_ccflow_with_notebook(parameters):
+    argv = [
+        "nbprint",
+        "--config-dir",
+        "examples/ccflow",
+        "+nbprint.notebook=examples/basic.ipynb",
+        "+nbprint.name=basic",
+        f"+context={parameters}",
+    ]
+    with patch.object(sys, "argv", argv):
+        hydra()
