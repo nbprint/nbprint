afa4556eea601e03532a6b7e905f4397e8128c26
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2026-08-16 12:00:04 -0400

    resolve copier update conflicts

--- js/tests/index.spec.js
+++ js/tests/index.spec.js
@@ -1,12 +1,5 @@
 import { test, expect } from "@playwright/test";
 
-<<<<<<< before updating
 test.describe("Basic tests", () => {
   test("exists", async ({ page }) => {});
-=======
-test.describe("Basics", () => {
-  test("basic", async () => {
-    await expect("").toBe("");
-  });
->>>>>>> after updating
 });
